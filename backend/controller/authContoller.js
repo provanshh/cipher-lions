d053f59d547fda6de670a813a4db67192649329a
Prince Pal <rishabpal2000@gmail.com>
2026-03-07 15:24:43 +0530

    fixed password saved as plain texts

--- backend/controller/authContoller.js
+++ backend/controller/authContoller.js
@@ -13,12 +13,12 @@ export const register = async (req, res) => {
     const existing = await Parent.findOne({ email });
     if (existing) return res.status(400).json({ message: "Email already registered" });
 
-    // const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const parent = await Parent.create({
       name,
       email,
-      password: password
+      password: hashedPassword
     });
     const token = generateToken(email)
 
@@ -36,9 +36,7 @@ export const login = async (req, res) => {
   try {
     const parent = await Parent.findOne({ email });
     if (!parent) return res.status(404).json({ message: "User not found" });
-const validPass=(password==parent.password)
-    // const validPass = await bcrypt.compare(password, parent.password);
-    // console.log(password," ", parent.password)
+    const validPass = await bcrypt.compare(password, parent.password);
     if (!validPass) return res.status(401).json({ message: "Invalid credentials" });
 
     const token = generateToken(email)
@@ -55,10 +53,6 @@ export const getParent=async(req,res)=>{
   const email=req.user.email;
   const parent=await Parent.findOne({email})
      const { name } = parent;
-    //  console.log(parent.email)
-    //  console.log(parent.name)
-    //  console.log(parent)
-
     res.json({ name, email });
   
 } catch (error) 
